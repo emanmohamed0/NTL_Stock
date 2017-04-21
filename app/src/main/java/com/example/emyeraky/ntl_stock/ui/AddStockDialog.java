6530d3fc7a453246025152b4da3417fe4cc01a40
Emy Eraky <emyeraky888@gmail.com>
2017-04-21 16:15:26 +0200

    stock

--- app/src/main/java/com/example/emyeraky/ntl_stock/ui/AddStockDialog.java
+++ app/src/main/java/com/example/emyeraky/ntl_stock/ui/AddStockDialog.java
@@ -5,7 +5,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -13,9 +18,11 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 
 import com.example.emyeraky.ntl_stock.R;
+import com.example.emyeraky.ntl_stock.widget.StockWidgetProvider;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -68,7 +75,37 @@ public class AddStockDialog extends DialogFragment {
     private void addStock() {
         Activity parent = getActivity();
         if (parent instanceof MainActivity) {
-            ((MainActivity) parent).addStock(stock.getText().toString());
+            String text = stock.getText().toString();
+            if (text != null && text.matches("[A-Za-z0-9 ]*")) {
+                ((MainActivity) parent).addStock(text);
+
+                RemoteViews remoteViews = new RemoteViews(this.getActivity().getPackageName(),R.layout.widget);
+
+                Intent clickIntent = new Intent(this.getActivity(),StockWidgetProvider.class);
+
+                Context context = getActivity();
+                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                ComponentName thisWidget = new ComponentName(context, StockWidgetProvider.class);
+                int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+
+                clickIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS,
+                        appWidgetIds);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(getActivity(), 0, clickIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+                remoteViews.setOnClickPendingIntent(R.id.widget_title, pendingIntent);
+                appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+
+//                StockWidgetProvider s = new StockWidgetProvider();
+//                Context context = getActivity();
+//                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+//                ComponentName thisWidget = new ComponentName(context, StockWidgetProvider.class);
+//                int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+//                appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.list_view);
+////                s.onUpdate(getContext(), AppWidgetManager.ACTION_APPWIDGET_UPDATE,R.id.list_view);
+////                appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.list_view);
+            }
         }
         dismissAllowingStateLoss();
     }
--- app/src/main/java/com/example/emyeraky/ntl_stock/widget/StockWidgetProvider.java
+++ app/src/main/java/com/example/emyeraky/ntl_stock/widget/StockWidgetProvider.java
@@ -3,9 +3,13 @@ package com.example.emyeraky.ntl_stock.widget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.example.emyeraky.ntl_stock.R;
 import com.example.emyeraky.ntl_stock.ui.DetailsActivity;
@@ -15,20 +19,78 @@ import com.example.emyeraky.ntl_stock.ui.MainActivity;
  * Created by Emy Eraky on 4/16/2017.
  */
 public class StockWidgetProvider extends AppWidgetProvider {
+//    public static final String TOAST_ACTION = "com.example.android.stackwidget.TOAST_ACTION";
+//    public static final String EXTRA_ITEM = "com.example.android.stackwidget.EXTRA_ITEM";
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals("android.net.conn.CONNECTIVITY_CHANGE")) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context.getApplicationContext());
+            ComponentName thisWidget = new ComponentName(context.getApplicationContext(), StockWidgetProvider.class);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+            if (appWidgetIds != null && appWidgetIds.length > 0) {
+                onUpdate(context, appWidgetManager, appWidgetIds);
+            }
+        }
         super.onReceive(context, intent);
+        //       Bundle extras = intent.getExtras();
+//        if(extras!=null) {
+//            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+//            ComponentName thisAppWidget = new ComponentName(context.getPackageName(), StockWidgetProvider.class.getName());
+//            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget);
+//
+//            onUpdate(context, appWidgetManager, appWidgetIds);
+//        }
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+//
+//        for (int i = 0; i < appWidgetIds.length; ++i) {
+//            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget);
+//            Intent intent = new Intent(context, StockRemoteViewsService.class);
+//            rv.setRemoteAdapter(appWidgetIds[i], R.id.list_view, intent);
+//
+//            rv.setEmptyView(R.id.list_view,R.id.empty_view);
+//
+//            //Sets up click listener for title to go to Main Activity
+//            Intent mainActivityIntent = new Intent(context, MainActivity.class);
+//            PendingIntent mainActivityPendingIntent = PendingIntent.getActivity
+//                    (context, 1, mainActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//            rv.setOnClickPendingIntent(R.id.widget_title, mainActivityPendingIntent);
+//
+//            //Sets up click listener for list items to go to detail view.
+//            Intent detailsIntent = new Intent(context, DetailsActivity.class);
+//            PendingIntent DetailsPendingIntent = PendingIntent.getActivity(context, 1, detailsIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//            rv.setPendingIntentTemplate(R.id.list_view, DetailsPendingIntent);
+//
+//            appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
+//        }
+//        super.onUpdate(context, appWidgetManager, appWidgetIds);
+//
+//}
 
+
+////////////////////
         for (int i = 0; i < appWidgetIds.length; ++i) {
-            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget);
-            Intent intent = new Intent(context, StockRemoteViewsService.class);
 
+            Intent intent = new Intent(context, StockRemoteViewsService.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget);
             rv.setRemoteAdapter(appWidgetIds[i], R.id.list_view, intent);
+
             rv.setEmptyView(R.id.list_view, R.id.empty_view);
+
+//            Intent toastIntent = new Intent(context, StockWidgetProvider.class);
+//            toastIntent.setAction(StockWidgetProvider.TOAST_ACTION);
+//            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+//            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            rv.setPendingIntentTemplate(R.id.list_view, toastPendingIntent);
 
             Intent mainActivityIntent = new Intent(context, MainActivity.class);
             PendingIntent mainActivityPendingIntent = PendingIntent.getActivity
@@ -40,6 +102,7 @@ public class StockWidgetProvider extends AppWidgetProvider {
             rv.setPendingIntentTemplate(R.id.list_view, DetailsPendingIntent);
 
             appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.list_view);
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
